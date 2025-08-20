f1b3929abba9e2f9dd682c142f3b72ace2367730
Ankita <ankitagondane1@gmail.com>
2025-08-20 08:54:35 +0530

    comments to understand

--- errorsTrial/InputValidator.java
+++ errorsTrial/InputValidator.java
@@ -1,7 +1,8 @@
 package errorsTrial;
 
 public class InputValidator  {
-
+    // using extends as this is method which may give the error, if on class level then entire class is an exception
+    // also as its checked execption the implementation will require the try and catch or the method throws the checkInputs again.
     public void validate(String input) throws CheckInputs{
         if(input == null){
             throw new CheckInputs(100);
